e9bf23c18c6efbb2aeb3e027a2131c2ad7dfe297
KeziahNjuguna <knjuguna@talinda.net>
2017-12-09 17:17:07 +0300

    add output/results  section

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,27 @@
 
 
-$(document).ready(function() {
-    $("button").click(function() {
-      var theAnswers = parseInt($("input#number").val());
-      (theAnswers);
-      theIntergers.forEach(function(theAnswer){
-      $("#result").append("<li>"+ theAnswer + "</li>");
+    var theIntergers =[];
+    function (theAnswer) {
+      for (var index = 1; index <=finisher; index++)
+            if (index %15===0) {
+        theIntergers.push("ping pong");
+        }else if (index % 5===0) {
+          theIntergers.push("pong");
+        } else if (index %3===0) {
+          theIntergers.push("ping");
+        }else {
+          theIntergers.push(index);
+        }
+        }
+
+
+    $(document).ready(function() {
+      $("#button").click(function() {
+        var theAnswer = parseInt($("#numberInput").val());
+        theAnswer;
+        theIntergers.forEach(function(theAnswer){
+          $("#results").append("<li>" +theAnswer + "</li>");
+        });
+
+      });
     });
-
-    });
-  });
-
-  var theIntergers = []
-  function (theAnswer) {
-    for (var index = 1; index <=theAnswer; index++)
-    if (index %15===0){
-      theIntergers.push("ping pong");
-    }else if (index %5===0){
-      theIntergers.push("pong");
-    }else if (index %3===0) {
-    }else {
-      theIntergers.push("index");
-    }
-
-    }
-
-
-
-
-    }

991b66d2217e503fa2eb2ba06dce984813f6eed5
KeziahNjuguna <knjuguna@talinda.net>
2017-12-10 13:40:32 +0300

    add numbers gif

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,25 @@
+var theIntergers =[];
+function game(theAnswer) {
+  for (var index = 1; index <=theAnswer; index++)
+        if (index %15===0) {
+  theIntergers.push("ping pong");
+    }else if (index % 5===0) {
+      theIntergers.push("pong");
+    } else if (index %3===0) {
+      theIntergers.push("ping");
+    }else {
+      theIntergers.push(index);
+    }
+    }
 
 
-    var theIntergers =[];
-    function (theAnswer) {
-      for (var index = 1; index <=finisher; index++)
-            if (index %15===0) {
-        theIntergers.push("ping pong");
-        }else if (index % 5===0) {
-          theIntergers.push("pong");
-        } else if (index %3===0) {
-          theIntergers.push("ping");
-        }else {
-          theIntergers.push(index);
-        }
-        }
-
-
-    $(document).ready(function() {
-      $("#button").click(function() {
-        var theAnswer = parseInt($("#numberInput").val());
-        theAnswer;
-        theIntergers.forEach(function(theAnswer){
-          $("#results").append("<li>" +theAnswer + "</li>");
-        });
-
-      });
+$(document).ready(function() {
+  $("#button").click(function() {
+    var theAnswer = parseInt($("#numberInput").val());
+    game(theAnswer);
+    theIntergers.forEach(function(theAnswer){
+      $("#results").append("<li>" +theAnswer + "</li>");
     });
+
+  });
+});
